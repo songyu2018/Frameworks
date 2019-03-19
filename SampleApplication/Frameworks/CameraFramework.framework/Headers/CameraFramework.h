b28f2eca1d4e5b9fe2e0442b71fddb74185ea904
Joe <joe.songyu@gmail.com>
2019-03-19 13:30:15 -0600

    Added two libs: 1. FSPagerView 2. SwipeCellKit
    Also enabled pull-to-refresh on one UITableView.

---
+++ SampleApplication/Frameworks/CameraFramework.framework/Headers/CameraFramework.h
@@ -0,0 +1,17 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
--- UtilityFramework/UtilityFramework/Extensions/Extensions.swift
+++ UtilityFramework/UtilityFramework/Extensions/Extensions.swift
@@ -119,6 +119,31 @@ public extension String {
 }
 
 
+// HEX support for UIColor.
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        assert(a >= 0.0 && a <= 1.0, "Invalid alpha component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: a)
+    }
+    
+    convenience init(rgb: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF,
+            a: a
+        )
+    }
+}
+//let color = UIColor(red: 0xFF, green: 0xFF, blue: 0xFF, a: 0.5)
+//let color2 = UIColor(rgb: 0xFFFFFF, a: 0.5)
+
+
+
 // https://medium.com/swift2go/swift-how-to-convert-html-using-nsattributedstring-8c6ffeb7046f
 public extension UIColor {
     
